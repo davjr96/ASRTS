5a419085ddd17973ce8159d169d459e4c4ba2ca1
Danny <davjr96@gmail.com>
2017-05-16 18:21:28 -0400

    Remove db.js from gitignore

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : "mongodb://admin:x2dGfFqYPhvCA8sB@cluster0-shard-00-00-bq4om.mongodb.net:27017,cluster0-shard-00-01-bq4om.mongodb.net:27017,cluster0-shard-00-02-bq4om.mongodb.net:27017/ASRTS?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+};
