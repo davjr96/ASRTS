348e6f4c39cd2c828b7cd2a17486c23d42e3eb91
Daniel Voce <danielvoce@Daniels-MacBook-Pro.local>
2017-06-16 16:10:27 -0700

    Update Ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'vassttimes.com',
-      key: '/home/danny/.ssh/ASRTS.pem',
+      key: '/Users/danielvoce/OneDrive/Documents/electronics/Timer/ASRTS/ASRTS.pem',
       ref: 'origin/master',
       repo: 'git@github.com:davjr96/ASRTS.git',
       path: '/home/ubuntu/ASRTS',
